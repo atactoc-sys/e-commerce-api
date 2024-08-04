172850aff307fd6b6c0f6b9f186cdee5d6353877
atactoc-sys <saikat.mandi2013@gmail.comm>
2024-08-05 01:11:56 +0530

    update

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ app.use(bodyParser.json());
  * Set up middleware to define a base URL path "/api" for the routes defined in the `productRoutes` module.
  * Any routes defined in the `productRoutes` module will be accessible under the "/api" path in the application.
  */
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to the E-Commerce API!\n\nHere are some details:\n- Get all products: /api/products\n- Delete product: /api/products/:id\n- update quantity: /api/products/:id/update_quantity\n- Create product: /api/products/create");
+  );
+});
+
+// Use product routes
+//app.use("/", productRoutes);
+
 app.use("/api", productRoutes);
 
 module.exports = app;
